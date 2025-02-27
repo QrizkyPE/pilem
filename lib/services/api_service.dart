6e6fd13b48a4d3581b1fe559a152c441a5a96609
Qrizky <107597860+QrizkyPE@users.noreply.github.com>
2025-02-27 21:38:28 +0700

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -9,20 +9,20 @@ class ApiService {
     final response = await http
         .get(Uri.parse("$baseUrl/movie/now_displaying?api_key=$apiKey"));
     final data = json.decode(response.body);
-    return List<Map<String, dynamic>>.from(data['result']);
+    return List<Map<String, dynamic>>.from(data['results']);
   }
 
   Future<List<Map<String, dynamic>>> getTrendingMovies() async {
     final response = await http
         .get(Uri.parse("$baseUrl/trending/movie/week?api_key=$apiKey"));
     final data = json.decode(response.body);
-    return List<Map<String, dynamic>>.from(data['result']);
+    return List<Map<String, dynamic>>.from(data['results']);
   }
 
   Future<List<Map<String, dynamic>>> getPopularMovies() async {
     final response =
         await http.get(Uri.parse("$baseUrl/movie/popular?api_key=$apiKey"));
     final data = json.decode(response.body);
-    return List<Map<String, dynamic>>.from(data['result']);
+    return List<Map<String, dynamic>>.from(data['results']);
   }
 }
